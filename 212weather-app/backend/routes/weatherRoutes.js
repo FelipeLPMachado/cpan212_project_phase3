f234325b12d498db898999bfee84cba6ca82a83a
FelipeLPMachado <felipelpmachado@gmail.com>
2024-12-13 02:17:58 -0500

    Delete 212weather-app directory

--- 212weather-app/backend/routes/weatherRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getWeather, getForecast } = require('../controllers/weatherController');
-
-const router = express.Router();
-
-router.get('/current', getWeather);
-router.get('/forecast', getForecast);
-
-module.exports = router;
--- 212weather-app/backend/utils/jwtHelper.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = process.env;
-
-const generateToken = (user) => {
-    return jwt.sign({ id: user._id, username: user.username }, JWT_SECRET, { expiresIn: '1h' });
-};
-
-module.exports = { generateToken };
